373eec98e49d31630c61140226ca1aaa5fafefc6
Joe Theuerkauf <joe@theuerkauf.dev>
2023-09-24 23:47:04 -0400

    Chop down long constructor signatures Fix tabs Switch Exception type hints to Throwable

--- src/BadRequestHttpException.php
+++ src/BadRequestHttpException.php
@@ -4,34 +4,51 @@ declare(strict_types=1);
 
 namespace Fuzz\HttpException;
 
+use Throwable;
+
 class BadRequestHttpException extends HttpException
 {
-	/**
-	 * HTTP status code
-	 *
-	 * @const int
-	 */
-	const HTTP_CODE = 400;
+    /**
+     * HTTP status code
+     *
+     * @const int
+     */
+    const HTTP_CODE = 400;
 
-	/**
-	 * Error code storage;
-	 *
-	 * @const string
-	 */
-	const ERROR = 'bad_request';
+    /**
+     * Error code storage;
+     *
+     * @const string
+     */
+    const ERROR = 'bad_request';
 
-	/**
-	 * BadRequestHttpException constructor.
-	 *
-	 * @param string|null     $errorDescription
-	 * @param array           $errorData
-	 * @param string|null     $userTitle
-	 * @param null            $userMessage
-	 * @param array           $headers
-	 * @param \Exception|null $previous
-	 */
-	public function __construct(string $errorDescription = null, array $errorData = [], string $userTitle = null, $userMessage = null, array $headers = [], \Exception $previous = null)
-	{
-		parent::__construct(self::HTTP_CODE, self::ERROR, $errorDescription, $errorData, $userTitle, $userMessage, $headers, $previous);
-	}
+    /**
+     * BadRequestHttpException constructor.
+     *
+     * @param string|null    $errorDescription
+     * @param array          $errorData
+     * @param string|null    $userTitle
+     * @param null           $userMessage
+     * @param array          $headers
+     * @param Throwable|null $previous
+     */
+    public function __construct(
+        string $errorDescription = null,
+        array $errorData = [],
+        string $userTitle = null,
+        $userMessage = null,
+        array $headers = [],
+        Throwable $previous = null
+    ) {
+        parent::__construct(
+            self::HTTP_CODE,
+            self::ERROR,
+            $errorDescription,
+            $errorData,
+            $userTitle,
+            $userMessage,
+            $headers,
+            $previous
+        );
+    }
 }
