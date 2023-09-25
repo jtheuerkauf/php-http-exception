4b6132970505c18c9edb9914b5a6d1e40b209747
Joe Theuerkauf <joe@theuerkauf.dev>
2023-09-24 23:47:04 -0400

    Chop down long constructor signatures Fix tabs Switch Exception type hints to Throwable

--- src/NotImplementedHttpException.php
+++ src/NotImplementedHttpException.php
@@ -4,34 +4,51 @@ declare(strict_types=1);
 
 namespace Fuzz\HttpException;
 
+use Throwable;
+
 class NotImplementedHttpException extends HttpException
 {
-	/**
-	 * HTTP status code
-	 *
-	 * @const int
-	 */
-	const HTTP_CODE = 501;
+    /**
+     * HTTP status code
+     *
+     * @const int
+     */
+    const HTTP_CODE = 501;
 
-	/**
-	 * Error code storage;
-	 *
-	 * @const string
-	 */
-	const ERROR = 'not_implemented';
+    /**
+     * Error code storage;
+     *
+     * @const string
+     */
+    const ERROR = 'not_implemented';
 
-	/**
-	 * NotImplementedHttpException constructor.
-	 *
-	 * @param string|null     $errorDescription
-	 * @param array           $errorData
-	 * @param string|null     $userTitle
-	 * @param null            $userMessage
-	 * @param array           $headers
-	 * @param \Exception|null $previous
-	 */
-	public function __construct(string $errorDescription = null, array $errorData = [], string $userTitle = null, $userMessage = null, array $headers = [], \Exception $previous = null)
-	{
-		parent::__construct(self::HTTP_CODE, self::ERROR, $errorDescription, $errorData, $userTitle, $userMessage, $headers, $previous);
-	}
+    /**
+     * NotImplementedHttpException constructor.
+     *
+     * @param string|null    $errorDescription
+     * @param array          $errorData
+     * @param string|null    $userTitle
+     * @param null           $userMessage
+     * @param array          $headers
+     * @param Throwable |null $previous
+     */
+    public function __construct(
+        string $errorDescription = null,
+        array $errorData = [],
+        string $userTitle = null,
+        $userMessage = null,
+        array $headers = [],
+        Throwable $previous = null
+    ) {
+        parent::__construct(
+            self::HTTP_CODE,
+            self::ERROR,
+            $errorDescription,
+            $errorData,
+            $userTitle,
+            $userMessage,
+            $headers,
+            $previous
+        );
+    }
 }
--- src/TooManyRequestsHttpException.php
+++ src/TooManyRequestsHttpException.php
@@ -4,39 +4,57 @@ declare(strict_types=1);
 
 namespace Fuzz\HttpException;
 
+use Throwable;
+
 class TooManyRequestsHttpException extends HttpException
 {
-	/**
-	 * HTTP status code
-	 *
-	 * @const int
-	 */
-	const HTTP_CODE = 429;
+    /**
+     * HTTP status code
+     *
+     * @const int
+     */
+    const HTTP_CODE = 429;
 
-	/**
-	 * Error code storage;
-	 *
-	 * @const string
-	 */
-	const ERROR = 'too_many_requests';
+    /**
+     * Error code storage;
+     *
+     * @const string
+     */
+    const ERROR = 'too_many_requests';
 
-	/**
-	 * TooManyRequestsHttpException constructor.
-	 *
-	 * @param int|null            $wait
-	 * @param string|null     $errorDescription
-	 * @param array           $errorData
-	 * @param string|null     $userTitle
-	 * @param null            $userMessage
-	 * @param array           $headers
-	 * @param \Exception|null $previous
-	 */
-	public function __construct($wait = null, string $errorDescription = null, array $errorData = [], string $userTitle = null, $userMessage = null, array $headers = [], \Exception $previous = null)
-	{
-		if (! is_null($wait)) {
-			$headers['Retry-After'] = $wait;
-		}
+    /**
+     * TooManyRequestsHttpException constructor.
+     *
+     * @param int|null       $wait
+     * @param string|null    $errorDescription
+     * @param array          $errorData
+     * @param string|null    $userTitle
+     * @param null           $userMessage
+     * @param array          $headers
+     * @param Throwable|null $previous
+     */
+    public function __construct(
+        $wait = null,
+        string $errorDescription = null,
+        array $errorData = [],
+        string $userTitle = null,
+        $userMessage = null,
+        array $headers = [],
+        Throwable $previous = null
+    ) {
+        if (!is_null($wait)) {
+            $headers['Retry-After'] = $wait;
+        }
 
-		parent::__construct(self::HTTP_CODE, self::ERROR, $errorDescription, $errorData, $userTitle, $userMessage, $headers, $previous);
-	}
+        parent::__construct(
+            self::HTTP_CODE,
+            self::ERROR,
+            $errorDescription,
+            $errorData,
+            $userTitle,
+            $userMessage,
+            $headers,
+            $previous
+        );
+    }
 }
